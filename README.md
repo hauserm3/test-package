bdd8b182ac5089b9cff641a4e18348b250b8986a
mike-3 <hauserm555@gmail.com>
2019-01-26 16:46:51 +0200

    commit msg

---
+++ README.md
@@ -0,0 +1,20 @@
+# test-package
+
+Test NPM package.
+
+## Installation
+
+```sh
+$ npm i test-package
+```
+
+## Using
+
+In Node.js:
+
+```sh
+const TestPackage = require('test-pakage');
+
+TestPackage.hello();
+TestPackage.proof();
+```
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello from npm test-package");
+const hello = function () {
+    console.log("Hello from npm test-package.");
+}
+
+const proof = function () {
+    console.log("Test-package module is connected.");
+}
+
+module.exports.hello = hello;
+module.exports.proof = proof;
